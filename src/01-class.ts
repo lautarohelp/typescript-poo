4967805f9d1015d3c7f3595910e539340978af90
lautarohelp <lautarotoledo391@gmail.com>
2024-01-29 12:01:52 -0600

    Class

---
+++ src/01-class.ts
@@ -0,0 +1,33 @@
+const date = new Date();
+date.getHours();
+date.getTime();
+date.toISOString();
+
+const date2 = new Date(2004, 10, 12);
+date2.getHours();
+date2.getTime();
+date2.toISOString();
+
+console.log(date);
+console.log(date2);
+
+let myVar;
+
+class MyDate {
+  year: number;
+  month: number;
+  day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+}
+
+const myDate = new MyDate(2004, 7, 7);
+
+console.log(myDate);
+
+
+
