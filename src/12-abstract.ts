b37b84840a13de28b73076fcbc8c907bdbce40e4
lautarohelp <lautarotoledo391@gmail.com>
2024-01-30 10:31:19 -0600

    Clases abstractas

---
+++ src/12-abstract.ts
@@ -0,0 +1,10 @@
+import { Animal, Dog } from "./08-inheritace";
+
+const animal = new Animal('borges');
+animal.greeting();
+
+const cheis = new Dog('cheis', 'lauti');
+cheis.greeting();
+cheis.woof(2)
+
+
