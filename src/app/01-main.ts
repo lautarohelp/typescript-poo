76b3eb9923d2a192f21dfc8a206cceec661843cd
lautarohelp <lautarotoledo391@gmail.com>
2024-01-30 11:19:34 -0600

    Tipando respuestas HTTP

---
+++ src/app/01-main.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { Product } from "./models/product.model";
+
+
+
+let anyVar: any = 12;
+let boolVar: boolean = anyVar;
+
+( async()=> {
+
+
+  async function getProducts(){
+    const { data } = await axios.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+    //code
+    return data;
+  }
+
+
+  const products = await getProducts();
+  console.log(products.map(item => `${item.id} - ${item.title}`));
+
+
+
+
+})();
