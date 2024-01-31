c7c8edb1c9fadae7c53479c52bda8de31b0190c3
lautarohelp <lautarotoledo391@gmail.com>
2024-01-31 11:02:44 -0600

    Generics en métodos

--- src/app/services/base-http.service.ts
+++ src/app/services/base-http.service.ts
@@ -2,17 +2,23 @@ import axios from "axios";
 
 import { Category } from "../models/category.model";
 import { Product } from "../models/product.model";
+import { UpdateProductDto } from "../dto/product.dto";
 
 export class BaseHttpService<TypeClass> {
 // private data: TypeClass[] = [];
 
   constructor(
-    private url: string
+    protected url: string
   ) {}
 
 async getAll() {
     const { data } = await axios.get<TypeClass[]>(this.url);
     return data
+}
+
+async update<ID, DTO>(id: ID, changes: DTO) {
+  const { data } = await axios.put(`${this.url}/${id}`, changes);
+  return data;
 }
 }
 
@@ -30,6 +36,9 @@ async getAll() {
 
   const rta = await productService.getAll()
   console.log('product', rta.length);
+  productService.update<Product['id'], UpdateProductDto>(1, {
+    title: 'Hola.'
+  });
 
   const url2 = 'https://api.escuelajs.co/api/v1/categories';
 
@@ -37,7 +46,6 @@ async getAll() {
 
   const rta1 = await categoryService.getAll()
   console.log('category', rta1.length);
-
 
 
 })()
